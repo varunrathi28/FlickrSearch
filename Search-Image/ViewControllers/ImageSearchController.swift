29dccc83f77f1681c09f3f3d196f3cc56099af19
Varun Rathi <varunrathi28@gmail.com>
2019-06-28 17:53:46 +0530

    Image Search Controller setup with functions and collection view, textfield extensions Search button function

--- Search-Image/ViewControllers/ImageSearchController.swift
+++ Search-Image/ViewControllers/ImageSearchController.swift
@@ -10,26 +10,90 @@ import UIKit
 
 class ImageSearchController: UIViewController {
 
-
+    //MARK: Outlets
     @IBOutlet weak var collectionView:UICollectionView!
     @IBOutlet weak var textfield:UITextField!
     @IBOutlet weak var btnSearch:UIButton!
     
-    var datasource:[AnyObject] = []
+    
+    
+    //MARK: Variables
+    
+    var currentRequest:URLSessionTask?
+    var currentPage = 1
+    var datasource:[FlickrPhotoModel] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpViews()
     }
+    
+    
+    func setUpViews(){
+        setupCollectionView()
+        registerNib()
+    
+    }
+    
+    func setupCollectionView(){
+    
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        textfield.delegate = self
+    }
+    
+   func registerNib(){
+        let cellNib = UINib.init(nibName:"FlickrImageCell", bundle: nil)
+        collectionView.register(cellNib, forCellWithReuseIdentifier:FlickerImageCell.reuseIdentifier)
+    }
+    
     
     @IBAction func searchButtonClicked(with sender:UIButton){
         
-        guard let text  = textfield.text else {
+        // If no input-> return
+        guard let query  = textfield.text else {
             return
         }
         
-        // Hit API Here
         
+        // If already requests are going on, cancel them all
+        if (currentRequest != nil){
+            currentRequest?.cancel()
+        }
+    
+        //collectionView.reloadData()
+        searchPhotos(for: query)
+        
+    }
+    
+    func searchPhotos(for query:String){
+        
+        guard query.count > 0 else {
+            return
+        }
+        
+        currentRequest = FlickerAPI.searchPhotosForKeywords(input: query, pageNo: currentPage, completionBlock: { (response, error) in
+            
+            guard let response  = response else {
+                
+                print("response incorrect")
+                return
+            }
+            
+            if self.currentPage == 1 {
+                self.datasource.removeAll()
+            }
+            
+            self.updateDataSource(models: response.photos.photo)
+        })
+        
+    }
+    
+    func updateDataSource(models:[FlickrPhotoModel]) {
+    
+        datasource.append(contentsOf: models)
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
     }
     
 }
