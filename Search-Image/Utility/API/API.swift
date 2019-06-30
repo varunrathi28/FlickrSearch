d611c136b714278826fd5795fa38891688b1f284
Varun Rathi <varunrathi28@gmail.com>
2019-06-30 18:41:25 +0530

    1 URL Logic for handling query params and percent encoding

--- Search-Image/Utility/API/API.swift
+++ Search-Image/Utility/API/API.swift
@@ -12,3 +12,32 @@ import Foundation
 public struct APIKey {
    static  let REST_API_KEY = "3e7cc266ae2b0e0d78e279ce8e361736"
 }
+
+
+public struct APIURLParameters{
+
+    static let scheme =     "https"
+    static let host =       "api.flickr.com"
+    static let path =       "/services/rest"
+}
+
+struct APIURLKeys {
+    
+    static let SearchMethod = "method"
+    static let APIKey = "api_key"
+    static let Extras = "extras"
+    static let ResponseFormat = "format"
+    static let DisableJSONCallback = "nojsoncallback"
+    static let SafeSearch = "safe_search"
+    static let Text = "text"
+    static let Page = "page"
+}
+
+
+public struct APIURLValues {
+static let SearchMethod = "flickr.photos.search"
+static let ResponseFormat = "json"
+static let DisableJSONCallback = "1"
+static let MediumURL = "url_m"
+static let SafeSearch = "1"
+}
--- Search-Image/Utility/API/FlickrAPI.swift
+++ Search-Image/Utility/API/FlickrAPI.swift
@@ -47,9 +47,10 @@ class FlickerAPI{
     class func searchPhotosForKeywords(input:String,pageNo: Int, completionBlock:@escaping (PhotoRequestBaseResponse?,Error?)-> Void)->URLSessionTask?{
         
         // TODO:- logic for creating url from query text and page id
-        let urlStr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(APIKey.REST_API_KEY)&format=json&nojsoncallback=1&safe_search=1&text=\(input)&page=\(pageNo)"
-        
-        guard let url = URL(string: urlStr) else {
+      //  let urlStr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(APIKey.REST_API_KEY)&format=json&nojsoncallback=1&safe_search=1&text=\(input)&page=\(pageNo)"
+      
+
+        guard let url = getImageURLFromParameters(query: input, page: pageNo) else {
             return nil
         }
         
@@ -79,6 +80,29 @@ class FlickerAPI{
         
         task.resume()
         return task
+    }
+    
+    class func getImageURLFromParameters(query: String,page :Int) -> URL? {
+        
+        var components = URLComponents()
+        components.scheme = APIURLParameters.scheme
+        components.host = APIURLParameters.host
+        components.path = APIURLParameters.path
+        
+        var queryItems = [URLQueryItem]()
+        
+        queryItems.append(URLQueryItem(name: APIURLKeys.SearchMethod, value: APIURLValues.SearchMethod))
+        queryItems.append(URLQueryItem(name: APIURLKeys.DisableJSONCallback, value: APIURLValues.DisableJSONCallback))
+        queryItems.append(URLQueryItem(name: APIURLKeys.APIKey, value: APIKey.REST_API_KEY))
+        queryItems.append(URLQueryItem(name: APIURLKeys.ResponseFormat, value: APIURLValues.ResponseFormat))
+        queryItems.append(URLQueryItem(name: APIURLKeys.SafeSearch, value: APIURLValues.SafeSearch))
+        queryItems.append(URLQueryItem(name: APIURLKeys.Text, value:query))
+        queryItems.append(URLQueryItem(name: APIURLKeys.Extras, value: APIURLValues.MediumURL))
+         queryItems.append(URLQueryItem(name: APIURLKeys.Page, value:String(page)))
+        
+        //Set query params
+        components.queryItems = queryItems
+        return components.url
     }
 
 
